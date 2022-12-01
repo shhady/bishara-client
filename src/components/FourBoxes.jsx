2b124fc062af09a4faea706a2f521d597957bd5d
shhady <shhadyse@gmail.com>
2022-12-01 14:04:15 +0200

    fourBoxes

---
+++ src/components/FourBoxes.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./FourBoxes.css";
+import { Link } from "react-router-dom";
+export default function FourBoxes() {
+  return (
+    <>
+      <div className="FourBoxesTop">
+        <div>
+          <h2>ابدأ الان</h2>{" "}
+        </div>
+        <Link to="/courses" style={{ textDecoration: "none" }}>
+          <div className="allInstruments" style={{ color: "black" }}>
+            جميع الالات
+          </div>
+        </Link>
+      </div>
+      <div className="fourboxesMain">
+        <Link to="/PianoPage" style={{ textDecoration: "none" }}>
+          <div className="box1">piano </div>
+        </Link>
+        <Link to="/OudPage" style={{ textDecoration: "none" }}>
+          <div className="box2">Oud</div>
+        </Link>
+        <Link to="/ViolinPage" style={{ textDecoration: "none" }}>
+          <div className="box3">Violin</div>
+        </Link>
+        <Link to="" style={{ textDecoration: "none" }}>
+          <div style={{ backgroundColor: "red" }} className="box4">
+            Guitar
+          </div>
+        </Link>
+      </div>
+    </>
+  );
+}
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,10 +4,12 @@ import LearnTrainCom from "./LearnTrainCom";
 import FreeVideos from "./FreeVideos";
 import About from "./About";
 import Footer from "./Footer";
+import FourBoxes from "./FourBoxes";
 export default function Home() {
   return (
     <div className="HomePage">
       <Hero />
+      <FourBoxes />
       <FreeVideos />
       <LearnTrainCom />
       <About />
