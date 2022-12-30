941f27e140e83e282b431bf93667d92a468ec203
shhady <shhadyse@gmail.com>
2022-12-30 17:59:34 +0200

    googleTesT

--- src/components/Auth/GoogleAuthSignUp.jsx
+++ src/components/Auth/GoogleAuthSignUp.jsx
@@ -2,10 +2,21 @@ import React from "react";
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
 
 const GoogleAuthSignUp = () => {
-  const onSuccess = (response) => {
-    console.log(response.email);
-    console.log(response.given_name);
-    console.log(response.family_name);
+  console.log(data.email);
+  console.log(data.given_name);
+  console.log(data.family_name);
+  const onSuccess = async (response) => {
+    const { data } = await axios.get(
+      "https://www.googleapis.com/oauth2/v2/userinfo",
+      {
+        headers: {
+          Authorization: `Bearer ${response.access_token}`,
+        },
+      }
+    );
+    console.log(data.email);
+    console.log(data.given_name);
+    console.log(data.family_name);
   };
 
   const onFailure = (error) => {
