d09bad77c4f13e444d309c89c2371819894924e7
shhady <shhadyse@gmail.com>
2022-12-30 18:07:33 +0200

    googleTesT

--- src/components/Auth/GoogleAuthSignUp.jsx
+++ src/components/Auth/GoogleAuthSignUp.jsx
@@ -1,22 +1,9 @@
 import React from "react";
 import { GoogleOAuthProvider, GoogleLogin } from "@react-oauth/google";
-import axios from "axios";
+
 const GoogleAuthSignUp = () => {
-  console.log(data.email);
-  console.log(data.given_name);
-  console.log(data.family_name);
-  const onSuccess = async () => {
-    const response = await axios.get(
-      "https://www.googleapis.com/oauth2/v2/userinfo",
-      {
-        headers: {
-          Authorization: `Bearer ${response.access_token}`,
-        },
-      }
-    );
-    console.log(response.data.email);
-    console.log(response.data.given_name);
-    console.log(response.data.family_name);
+  const onSuccess = (response) => {
+    console.log(response);
   };
 
   const onFailure = (error) => {
@@ -26,7 +13,7 @@ const GoogleAuthSignUp = () => {
     <GoogleOAuthProvider clientId="623673237970-h7oi3db0ime9917tl4gu24on8j6c5vvf.apps.googleusercontent.com">
       <GoogleLogin
         scope="https://www.googleapis.com/auth/userinfo.email"
-        redirectUri="https://bishara.netlify.app"
+        redirectUri="https://bishara.netlify.app/"
         onSuccess={onSuccess}
         onFailure={onFailure}
       >
