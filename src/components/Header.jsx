b9409798aa1920f90a1e607c53dafe7b3e1e9e43
shhady <shhadyse@gmail.com>
2023-01-18 16:06:27 +0200

    logo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -947,7 +947,7 @@ export default function Header({
                 }}
                 onClick={() => setUpdateComponent("")}
               >
-                FUNAN<b>.AI</b>
+                <b>FU</b>NAN
               </div>{" "}
             </Link>
           </div>
@@ -974,7 +974,7 @@ export default function Header({
                 X
               </h3>
               <span style={{ fontSize: "30px", color: "white" }}>
-                FUNAN<b>.AI</b>
+                <b>FU</b>NAN
               </span>
               <span style={{ color: "black" }}>X</span>
             </div>
