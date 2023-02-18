a986b3480ed2c584511b4bf6b9897afbacedefc9
shhady <shhadyse@gmail.com>
2023-02-18 11:02:30 +0200

    logout

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -6,6 +6,7 @@ const authReducer = (state = { authData: null }, action) => {
 
     case "LOGOUT":
       localStorage.removeItem("profile");
+      localStorage.clear();
       return { ...state, authData: null };
     default:
       return state;
