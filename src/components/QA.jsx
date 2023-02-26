8ba33d839cd83a5c36e75df43af90ec1aef4111f
shhady <shhadyse@gmail.com>
2023-02-26 12:13:18 +0200

    hero and QA

--- src/components/QA.jsx
+++ src/components/QA.jsx
@@ -2,20 +2,58 @@ import React from 'react';
 
 export default function QA() {
   return (
-    <div style={{marginTop:"100px", textAlign:"center"}}>
-      <h1>أسئلة متكرره</h1> 
-      <details>
-    <summary>
-        tell me about yourself?
+    <div style={{ textAlign:"center", width:"80%", margin:"auto"}}>
+      <h1 style={{marginTop:"100px"}}>أسئلة متكرره</h1> 
+      <details style={{border: "1px solid black"}}>
+    <summary style={{fontWeight:"bold", fontSize:"18px", cursor:"pointer"}}> 
+    ما هي الآلات الموسيقية العربية التي يمكن تعلمها في هذا الموقع؟
     </summary>
-    nice nice 
+    يمكن تعلم العزف على العود والقانون والناي والكمان والطبلة والدربكة والبيانو وغيرها من الآلات الموسيقية العربية.
+ 
 </details>
-<details>
-    <summary>
-        ask me anything...
+<details  style={{border: "1px solid black"}}>
+    <summary  style={{fontWeight:"bold", fontSize:"18px", cursor:"pointer"}} >
+    هل يتوفر الموقع على دروس مصورة ومادة صوتية لتسهيل عملية التعلم؟
     </summary>
-    what ?
+    نعم، يوفر الموقع دروساً مصورة ومادة صوتية عالية الجودة لتسهيل عملية التعلم وتحسين مهارات العزف.
 </details>
+<details  style={{border: "1px solid black"}}>
+    <summary  style={{fontWeight:"bold", fontSize:"18px", cursor:"pointer"}}>
+    هل يمكن للمبتدئين تعلم العزف عبر هذا الموقع؟
+    </summary>
+    نعم، يمكن للمبتدئين تعلم العزف على الآلات الموسيقية بسهولة عبر دروس الموقع المصممة خصيصاً لهم..
+</details>
+<details  style={{border: "1px solid black"}}>
+    <summary  style={{fontWeight:"bold", fontSize:"18px", cursor:"pointer"}}>
+    هل يوفر الموقع دورات تدريبية خاصة بكل طالب؟
+    </summary>
+    نعم، يوفر الموقع دورات تدريبية خاصة لكل طالب عبر الإنترنت مع مدربين محترفين وذوي خبرة في العزف على الآلات الموسيقية العربية.
+
+</details>
+<details  style={{border: "1px solid black"}}>
+    <summary  style={{fontWeight:"bold", fontSize:"18px", cursor:"pointer"}}>
+     ما هي المؤهلات التي يجب أن يمتلكها المدربون في موقع تعليم العزف على الآلات الموسيقية؟
+
+    </summary>
+    يجب على المدربين أن يكونوا ذوي خبرة في مجال العزف على الآلات الموسيقية، وأن يمتلكوا معرفة متقدمة بالنسبة للآلات التي يعلمونها، وأن يكونوا قادرين على توجيه الطلاب بشكل فعال وتقديم النصائح القيمة لتحسين مهاراتهم.
+
+</details>
+<details  style={{border: "1px solid black"}}>
+    <summary  style={{fontWeight:"bold", fontSize:"18px", cursor:"pointer"}}>
+    هل يمكنني الاستفادة من الموقع لتحسين مهاراتي العزف على آلة موسيقية محددة؟
+
+    </summary>
+    نعم، يمكن للطلاب الاستفادة من الموقع لتحسين مهاراتهم في العزف على آلات موسيقية محددة، مثل البيانو أو الجيتار أو الكمان أو العود وغيرها. يوفر الموقع دروسًا خاصة بكل آلة، ويستخدم أفضل الممارسات والتقنيات للتدريب على العزف الصحيح والفعال.
+
+</details>
+<details  style={{border: "1px solid black"}}>
+    <summary  style={{fontWeight:"bold", fontSize:"18px", cursor:"pointer"}}>
+     هل يمكنني التفاعل مع المدربين لطرح الأسئلة والحصول على المساعدة اللازمة؟
+
+    </summary>
+     نعم، يمكن للطلاب التفاعل مع المدربين من خلال منصة الدروس المباشرة لطرح الأسئلة والحصول على المساعدة اللازمة. كما يمكن للطلاب إرسال فيديو عزف والحصول على رد شخصي من قبل المعلم
+</details>
+
 
 </div>
   )
--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -3,19 +3,19 @@ export const sliderData = [
     image:
       "https://images.pexels.com/photos/3120109/pexels-photo-3120109.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     heading: "كمان",
-    desc: "",
+    desc: "تعلم فن العزف على الكمان الشرقي واكتشف جوانب جديدة من موهبتك الموسيقية مع دورتنا الشيقة والممتعة!",
   },
   {
     image:
       "https://images.pexels.com/photos/1246437/pexels-photo-1246437.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
     heading: "بيانو",
-    desc: "",
+    desc: "استكشف عالم الإبداع الموسيقي وابدأ رحلتك في فن العزف على البيانو مع دورتنا الممتعة والمصممة خصيصاً للطالب العربي المبتدئ والمتقدم على حد سواء!",
   },
   {
     image:
       "https://images.pexels.com/photos/1751731/pexels-photo-1751731.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
-    heading: "جيثار",
-    desc: "",
+    heading: "جيتار",
+    desc: "نضم إلى دورتنا المميزة لتعلم فن العزف على الجيتار، واكتشف جمال الموسيقى العربية والروك والبلوز والكلاسيكية والعديد من الأنواع الأخرى مع مدربين محترفين وتقنيات تعليمية حديثة",
   },
   //   {
   //     image:
