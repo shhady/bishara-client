6c9482fdd193532cab4c58f5d999b6e22e10bba9
shhady <shhadyse@gmail.com>
2023-07-24 01:42:10 +0300

    scroll

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import FreeVideos from "../../components/FreeVideos";
 import About from "../../components/About/About";
 import Footer from "../../components/Footer";
@@ -7,9 +7,19 @@ import TeachersHomePage from "../../components/TeachersHomePage";
 import CoursesHomePage from "../../components/CoursesHomePage";
 import NewHero from "../../components/newHero/NewHero";
 import ContactUs from "../../components/ContactUs";
-export default function Home({
-  user
-}) {
+export default function Home({user, setUser}) {
+
+  useEffect(() => {
+    if(user.role === "admim" || user.role === "teacher") return;
+    // Check if user exists and has a valid createdAt date
+    if (user && new Date(user.createdAt) < new Date('2023-07-23T00:00:00Z')) {
+      // Clear local storage
+      window.localStorage.removeItem('profile');
+      window.localStorage.removeItem('token');
+      setUser(null);
+    }
+  }, [user, setUser]);
+
   return (
     <div className="HomePage">
       <NewHero user={user}/>
