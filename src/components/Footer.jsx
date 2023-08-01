f1d81f17981145f4f057436c8a1ee40ec365c325
shhady <shhadyse@gmail.com>
2023-08-01 14:40:00 +0300

    social media icons

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,11 @@
 import React , {useState}from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import {
-  faEnvelope
-} from "@fortawesome/free-solid-svg-icons";
+
 import "./footer.css";
 import {Link} from "react-router-dom"
 import SocialMedia from "./socialMedia/SocialMedia";
+import { FaInstagram, FaFacebook } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
+
 export default function Footer() {
   const [showContactUs, setShowContactUs]=useState(false)
   function sendEmail() 
@@ -32,9 +32,12 @@ export default function Footer() {
         <div onClick={()=>setShowContactUs(!showContactUs)} style={{cursor:"pointer"}}>تواصل معنا</div> 
         {showContactUs ? (
           <div onClick={()=>setShowContactUs(!showContactUs)} style={{position:"fixed",height:"100vh", width:"100vw", background:"rgba(98, 98, 100, 0.3)", top:"0", left:"0", display:"flex", justifyContent:"center",alignItems:"center"}}>
-          <div style={{position:"fixed",height:"40vh", width:"40vw",maxWidth:"300px",border: "1px solid black", backgroundColor:"white", zIndex:"15", display:"flex",flexDirection:"column", justifyContent:"space-around",alignItems:"center"}}>
+          <div style={{position:"fixed",height:"40vh", width:"40vw",maxWidth:"300px",border: "1px solid black", backgroundColor:"white", zIndex:"15", display:"flex",flexDirection:"column", justifyContent:"space-around",alignItems:"flex-start"}}>
           {/* <div><a href="tel:0587230238"  style={{textDecoration:"none", color:"black"}}><FontAwesomeIcon icon={faPhone}/>  هاتف</a></div> */}
-          <div onClick={()=>sendEmail()} style={{cursor:"pointer"}}><FontAwesomeIcon icon={faEnvelope}/>  بريد الكتروني</div>
+          <div onClick={()=>sendEmail()} style={{display:'flex', justifyContent:'center', alignItems:"center", gap:"5px", textDecoration:"none",color:"black"}}><MdEmail className="iconEmail"/> Email</div>
+         <div > <a href="https://www.instagram.com/funanmusic/" target="_blank" rel="noreferrer" style={{display:'flex', justifyContent:'center', alignItems:"center", gap:"5px", textDecoration:"none",color:"black"}}><FaInstagram className="iconInstagram"/>Instagram</a> </div>
+        {/* <a href="https://www.youtube.com/watch?v=DzsU1aXGoGg" target="_blank" rel="noreferrer" style={{height:'40px'}}><FaYoutubeSquare className="iconYoutube" /></a> */}
+        <div> <a href="https://www.facebook.com/Funan.org" target="_blank" rel="noreferrer" style={{display:'flex', justifyContent:'center', alignItems:"center", gap:"5px", textDecoration:"none",color:"black"}} ><FaFacebook className="iconFacebook" />Facebook</a></div>
           </div> 
           </div> 
         ):(null)}
--- src/components/socialMedia/SocialMedia.jsx
+++ src/components/socialMedia/SocialMedia.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
 import "./socialmedia.css";
-import { FaInstagram, FaFacebook, FaYoutubeSquare } from 'react-icons/fa';
+import { FaInstagram, FaFacebook, FaYoutubeSquare,FaTiktok } from 'react-icons/fa';
+import { MdEmail } from 'react-icons/md';
+
 
 export default function SocialMedia() {
+    function sendEmail() 
+    {
+        window.location.assign("mailto:funanmusic@gmail.com");
+    }
   return (
     <div>
       <div className="social-icons">
+        <div style={{height:'40px'}}><MdEmail className="iconEmail"  onClick={()=>sendEmail()}/></div>
         <a href="https://www.instagram.com/funanmusic/" target="_blank" rel="noreferrer" style={{height:'40px'}} ><FaInstagram className="iconInstagram"/></a>
         <a href="https://www.youtube.com/watch?v=DzsU1aXGoGg" target="_blank" rel="noreferrer" style={{height:'40px'}}><FaYoutubeSquare className="iconYoutube" /></a>
         <a href="https://www.facebook.com/Funan.org" target="_blank" rel="noreferrer" style={{height:'40px'}}><FaFacebook className="iconFacebook" /></a>
-        
+        <a href="https://www.tiktok.com/@funanmusic?_t=8eTUH98SnUv&_r=1" target="_blank" rel="noreferrer" style={{height:'40px'}}><FaTiktok className="iconTiktok" /></a>
       </div>
     </div>
   );
